836ea5de45e6383d81b9889cf1b433a72cd5c9ea
Thomas Levine <_@thomaslevine.com>
2014-06-17 19:51:54 -0400

    grouping

--- wbcontractawards/parse.py
+++ wbcontractawards/parse.py
@@ -8,23 +8,16 @@ def search(response):
     html.make_links_absolute(response.url)
     return map(str, html.xpath("//ol[@id='search-results']/li/h3/a/@href"))
 
-def _contract_parser():
-    status = p.oneOf(['awarded','evaluated','rejected'], caseless = True)
-    bidder = p.Suppress(p.CaselessLiteral('bidder'))
-    to_newline = p.SkipTo(p.oneOf(['\n','<','\r']))
-    name = p.Suppress(p.SkipTo(p.Word('Name'))) + p.Suppress(p.Word('Name: ')) + to_newline
-    price = p.Suppress(p.SkipTo(p.CaselessLiteral('price'))) + \
-            p.Suppress(p.SkipTo(':')) + \
-            p.Word(p.alphas) + p.Word(p.nums + ',')
-    parser = p.Suppress(p.SkipTo(status)) + status + bidder + name + price
-    return p.ZeroOrMore(parser)
-
 def contract(response):
-    html = fromstring(re.sub(r'<br', '\n<br', response.text, flags = re.IGNORECASE))
+    html = fromstring(re.sub(r'<', '\n<', response.text, flags = re.IGNORECASE))
     text = html.xpath('//div[@class="prc_notice"]')[0].text_content().strip()
-    return text
-    parser = _contract_parser()
-    matches = parser.parseString(text)
-    print(matches)
-   #if len(matches) > 0:
-   #    return [{'status':status, 'name': name} for status, name in zip(matches[0::2], matches[1::2])]
+    bidder = None
+    for line in text.split('\n'):
+        if re.search(r'bidder', line, flags = re.IGNORECASE):
+            if bidder != None:
+                yield bidder
+            bidder = {'status': re.match(r'^([a-z]+) ', line, flags = re.IGNORECASE).group(1)}
+        elif bidder != None:
+            m = re.match(r'( *[^:]+ *):( *[^:]+ *)', line)
+            if m:
+                bidder[m.group(1).lower()] = m.group(2)
