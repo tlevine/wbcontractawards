b3706b088dc6d0a6395711641af20d033f06b125
Thomas Levine <_@thomaslevine.com>
2014-06-17 20:23:27 -0400

    fixture

--- wbcontractawards/test/test_parse.py
+++ wbcontractawards/test/test_parse.py
@@ -7,4 +7,7 @@ import wbcontractawards.parse as p
 def test_OP00027656():
     with open(os.path.join('wbcontractawards','test','fixtures','OP00027656.txt')) as fp:
         text = fp.read()
-    n.assert_list_equal(list(p.bidders(text)), [])
+    observed = list(p.bidders(text))
+    with open(os.path.join('wbcontractawards','test','fixtures','OP00027656.py'), 'w') as fp:
+        fp.write(str(observed))
+   #n.assert_list_equal(, [])
