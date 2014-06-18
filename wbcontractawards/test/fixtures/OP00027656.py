05408b4165f4f01c72b698407c256fb25817790d
Thomas Levine <_@thomaslevine.com>
2014-06-17 20:26:32 -0400

    reorganize test

--- wbcontractawards/test/fixtures/OP00027656.py
+++ wbcontractawards/test/fixtures/OP00027656.py
@@ -1,4 +1,4 @@
-[
+observed = [
     {
         'contract price': ' VND42,882,861,177.00',
         'address': ' ',
@@ -30,3 +30,54 @@
         'status': 'Rejected',
         'country': ' Vietnam'}
 ]
+
+text = '''
+ICB Contract Award Notice
+Vietnam
+
+Project: P125996 - Distribution Efficiency Project
+
+Bid/Contract Reference No: Package NPC/DEP2.4-110-G02
+Scope of Contract: Procurement and transportation of materials and equipment for 110kV substations: Luc Nam, Hoa Binh, Van Ban,Gian Khau, Kim Son 110kV substations
+Duration of Contract: 20Week(s)
+Contract Signature Date: 
+Evaluation Currency: VND-Vietnamese Dong
+
+
+Awarded Bidder(s):
+
+Name: Consortium EnTec - Cooper
+Address: 
+Country: Vietnam
+Bid Price at Opening:USD 2,108,523.94 
+Evaluated Bid Price: VND42,882,861,177.00
+Contract Price: VND42,882,861,177.00
+
+
+ 
+Evaluated Bidder(s):
+
+Name : LS Vina Industrial System
+Address :
+Country: Vietnam
+Bid Price at Opening:USD 1,802,564.00 
+Evaluated Bid Price: VND38,052,126,040.00
+
+ 
+Rejected Bidder(s):
+
+Name: EDH - TMD Bidder Joint Venture
+Address: 
+Country: Vietnam
+Bid Price at Opening:VND 50,944,928,551.00 
+Reason for Rejection: Non Responsive
+
+
+Rejected Bidder(s):
+
+Name: Viet A Group Holdings Co.
+Address: 
+Country: Vietnam
+Bid Price at Opening:USD 2,335,870.61 
+Reason for Rejection: Non Responsive
+'''
--- wbcontractawards/test/test_parse.py
+++ wbcontractawards/test/test_parse.py
@@ -5,8 +5,6 @@ import nose.tools as n
 import wbcontractawards.parse as p
 
 def test_OP00027656():
-    with open(os.path.join('wbcontractawards','test','fixtures','OP00027656.txt')) as fp:
-        text = fp.read()
+    from wbcontractawards.test.fixtures.OP00027656 import text, expected
     observed = list(p.bidders(text))
-    from wbcontractawards.test.fixtures.parse import OP00027656 as expected
-   #n.assert_list_equal(, [])
+    n.assert_list_equal(observed, expected)
